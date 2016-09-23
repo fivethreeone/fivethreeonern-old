43451da32ef6fcf7f4615a0a69b3fbd9ffe2863f
Frank Wang <frank.wangtao@gmail.com>
2016-09-23 19:15:09 -0400

    Add basic sql query functions

--- src/fivethreeonern/sqlite.cljs
+++ src/fivethreeonern/sqlite.cljs
@@ -6,3 +6,17 @@
 (defstate sqlite
   :start (.openDatabase node-module #js {:name "531" :createFromLocation 1} #(js/console.log "sql ok") #(js/console.log "sql error")))
 
+(defn execute-sql [tx final-cb [query & other-queries]]
+  (.executeSql tx query #js [] (fn [tx results]
+                                 (if (empty? other-queries)
+                                   (let [results (-> results .-rows .raw js->clj)]
+                                     (final-cb results))
+                                   (execute-sql tx final-cb other-queries)))))
+
+(defn transaction [query-strings final-cb]
+  (.transaction @sqlite
+                (fn [tx]
+                  (execute-sql tx final-cb query-strings))))
+
+(defn query [query-str cb]
+  (transaction [query-str] cb))
